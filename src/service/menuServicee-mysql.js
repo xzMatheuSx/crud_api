133cd44af91475e14a98d3430336f1e10efd66d7
xzMatheuSx <matheusaugusth@gmail.com>
2023-09-06 22:00:36 -0300

    correnção do find

--- src/service/menuServicee-mysql.js
+++ src/service/menuServicee-mysql.js
@@ -1,36 +1,36 @@
-const connection = require('./db-mysql');
+// const connection = require('./db-mysql');
 
-const findAll = async () => {
-    const db = await connection();
-    const [rows] = await db.query("SELECT * FROM menu_products;");
-    return rows;
-}
+// const findAll = async () => {
+//     const db = await connection();
+//     const [rows] = await db.query("SELECT * FROM menu_products;");
+//     return rows;
+// }
 
-const find = async (id) => {
-    const db = await connection();
-    const [menu] = await db.query("SELECT * FROM menu_products WHERE id = ?", [id]);
-    return (menu[0] ?? null);
-};
+// const find = async (id) => {
+//     const db = await connection();
+//     const [menu] = await db.query("SELECT * FROM menu_products WHERE id = ?", [id]);
+//     return (menu[0] ?? null);
+// };
 
-const create = async (id, name, price, menu, extras) => {
-    if (!id || !name || !price || !menu || !extras) return false;
+// const create = async (id, name, price, menu, extras) => {
+//     if (!id || !name || !price || !menu || !extras) return false;
 
-    let newMenu;
-    try {
-        const db = await connection();
-        const result = await db.query("INSERT INTO menu_products (id, name, price, menu, extras) VALUES (?, ?, ?, ?, ?)", [id, name, price, menu, extras]);
-        newMenu = {id, name, price, menu, extras};
-    } catch(e) {
-        console.log(e);
-        return false;
-    }
+//     let newMenu;
+//     try {
+//         const db = await connection();
+//         const result = await db.query("INSERT INTO menu_products (id, name, price, menu, extras) VALUES (?, ?, ?, ?, ?)", [id, name, price, menu, extras]);
+//         newMenu = {id, name, price, menu, extras};
+//     } catch(e) {
+//         console.log(e);
+//         return false;
+//     }
 
-    return newMenu;
-}
+//     return newMenu;
+// }
 
-module.exports = {
-    findAll,
-    find,
-    create,
+// module.exports = {
+//     findAll,
+//     find,
+//     create,
     
-}
+// }
